b25db3b3c2f715395a19926eb12d1c48f7d22844
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2025-03-14 07:39:53 -0600

    Add files via upload

---
+++ imprimirvalores.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    // Creo mis variables:
+    string nombre = "Juan";
+    short int edad = 34;
+    cout << "El nombre de " << nombre << " de edad " << 34;
+    return 0;
+}
